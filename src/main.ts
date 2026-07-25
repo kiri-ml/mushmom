84646cbc232123455937961b66cd51d59c8eb17d
Kirisame Marisa <279059590+kiri-ml@users.noreply.github.com>
2026-07-25 15:10:21 +0800

    feat: add languages icon to language selector

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "./styles.css";
+import { createIcons, Languages } from "lucide";
 import { initI18n } from "./i18n/index";
 import "./load";
 import { initApp } from "./app";
+
+createIcons({ icons: { Languages } });
 
 async function bootstrap(): Promise<void> {
   await initI18n();
